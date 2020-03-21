d5b82a23ba5ab77d1f6fa4158eb5268002304094
Leonardo Ribeiro Solano <61062447+lrsolano@users.noreply.github.com>
2020-03-21 03:50:16 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from plotly.subplots import make_subplots
 from random import randint
 
 #Gráficos relacionados a um país
-def covid_19_country(country='Brazil'):
+def covid_19_country(country='Brazil',extra=1):
 
     #cria os datasets já organizados    
     confirmed, deaths, recovered = treatment(country)
@@ -14,6 +14,13 @@ def covid_19_country(country='Brazil'):
     #cria a figura para plotar o grafico com 3 linhas e uma coluna
     fig = make_subplots(rows=3, cols=1,
                         subplot_titles=("Confirmed", "Deaths", "recovered"))
+    #fator de aumento caso não se tenha certeza dos valores divulgados
+    if extra == 1:
+        for k in (1.05,1.10,1.20,1.30,1.50):
+            fig.add_trace(go.Scatter(x=list(confirmed.index), 
+                           y=list(confirmed.values*k),
+                           mode='lines+markers',
+                           name='{}'.format(k)), row=1,col=1)
     
     #plota cada um dos gráficos
     fig.add_trace(go.Scatter(x=list(confirmed.index), 
@@ -166,6 +173,77 @@ def treatment(country):
     recovered = recovered.sum()
     
     return confirmed, deaths, recovered
+
+def extrapolate(country='Brazil'):
+
+    #cria os datasets já organizados    
+    confirmed, deaths, recovered = treatment(country)
+    
+    #Chama a função para listar apenas quando começar a aparecer casos confirmados e começar a contagem de dias
+    c_data, dc = lists(confirmed)
+    d_data, dd = lists(deaths)
+    r_data, dr = lists(recovered)
+    
+    #cria a imagem para ser plotada com 3 linhas e 1 coluna
+    fig = make_subplots(rows=3, cols=1,
+                        subplot_titles=("Confirmed", "Deaths", "recovered"))
+        
+    #adiciona o grafico de confirmados do país a imagem
+    fig.add_trace(go.Scatter(x=dc, 
+                           y=c_data,
+                           mode='lines+markers',
+                           name=country), row=1,col=1)
+    #adiciona o grafico de fatais do país a imagem
+    fig.add_trace(go.Scatter(x=dd, 
+                           y=d_data,
+                           mode='lines+markers',
+                           name=country), row=2,col=1)
+        #adiciona o grafico de recuperados do país a imagem
+    fig.add_trace(go.Scatter(x=dr, 
+                           y=r_data,
+                           mode='lines+markers',
+                           name=country), row=3,col=1)
+    
+    #fator de aumento por dia
+    for k in (1.05,1.10,1.15,1.20):
+        extrapolator_c,future_c  = extrapoletor(c_data,dc,k)
+        extrapolator_d, future_d = extrapoletor(d_data,dd,k)
+        extrapolator_r, future_r = extrapoletor(r_data,dr,k)
+        
+        #Adiciona as funções aos graficos
+        fig.add_trace(go.Scatter(x=future_c, 
+                           y=extrapolator_c,
+                           mode='lines+markers',
+                           name='k={}'.format(k)), row=1,col=1)
+        fig.add_trace(go.Scatter(x=future_d, 
+                           y=extrapolator_d,
+                           mode='lines+markers',
+                           name='k={}'.format(k)), row=2,col=1)
+        fig.add_trace(go.Scatter(x=future_r, 
+                           y=extrapolator_r,
+                           mode='lines+markers',
+                           name='k={}'.format(k)), row=3,col=1)
+    #coloca o gráfico em base logarítmica
+    fig.update_layout(xaxis_type="log", yaxis_type="log")
+    #salva a imagem
+    fig.write_html('fig_comparation.html', auto_open=True)
+
+        
+
+def extrapoletor(data,days,plus):
+    today = data[-1]
+    extra = [today]
+    for x in range(30):
+        i = today*plus
+        extra.append(i)
+        today = i
+    days = list(range(len(days),len(days)+30))
+    return extra, days
+        
+        
+        
+    
+    
     
     
     
