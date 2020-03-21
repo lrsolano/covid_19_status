1d4dd44ef85271cee0e2562a4d5cdc416c2566bf
Leonardo Ribeiro Solano <61062447+lrsolano@users.noreply.github.com>
2020-03-21 14:04:24 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from plotly.subplots import make_subplots
 from random import randint
 
 #Gráficos relacionados a um país
-def covid_19_country(country='Brazil',extra=1):
+def covid_19_country(country='Brazil',extra=0):
 
     #cria os datasets já organizados    
     confirmed, deaths, recovered = treatment(country)
@@ -129,6 +129,67 @@ def covid_19_comparation(countrys=['Brazil','Japan','China','Italy','US']):
     fig.update_layout(xaxis_type="log", yaxis_type="log")
     #salva a imagem
     fig.write_html('fig_comparation.html', auto_open=True)
+
+#Função para extrapolar os casos    
+def extrapolate(country='Brazil',plus=1,future=30):
+
+    #cria os datasets já organizados    
+    confirmed, deaths, recovered = treatment(country)
+    
+    #Chama a função para listar apenas quando começar a aparecer casos confirmados e começar a contagem de dias
+    c_data, dc = lists(confirmed)
+    d_data, dd = lists(deaths)
+    r_data, dr = lists(recovered)
+    
+    #cria a imagem para ser plotada com 3 linhas e 1 coluna
+    fig = make_subplots(rows=3, cols=1,
+                        subplot_titles=("Confirmed", "Deaths", "recovered"))
+        
+    #adiciona o grafico de confirmados do país a imagem
+    fig.add_trace(go.Scatter(x=dc, 
+                           y=c_data,
+                           mode='lines+markers',
+                           name=country), row=1,col=1)
+    #adiciona o grafico de fatais do país a imagem
+    fig.add_trace(go.Scatter(x=dd, 
+                           y=d_data,
+                           mode='lines+markers',
+                           name=country), row=2,col=1)
+        #adiciona o grafico de recuperados do país a imagem
+    fig.add_trace(go.Scatter(x=dr, 
+                           y=r_data,
+                           mode='lines+markers',
+                           name=country), row=3,col=1)
+    
+    try:
+        #Extrapola confirmados
+        extrapolator_c,future_c, media = extrapoletor(c_data,dc,future,plus)
+        media = round((media-1)*100)
+        fig.add_trace(go.Scatter(x=future_c, 
+                           y=extrapolator_c,
+                           mode='lines+markers',
+                           name='Confirmed {}%'.format(media)), row=1,col=1)
+        #Extrapola fatais
+        extrapolator_d, future_d, media = extrapoletor(d_data,dd,future,plus)
+        media = round((media-1)*100)
+        fig.add_trace(go.Scatter(x=future_d, 
+                           y=extrapolator_d,
+                           mode='lines+markers',
+                           name='Deaths {}%'.format(media)), row=2,col=1)
+        #Extrapola recuperados
+        extrapolator_r, future_r, media = extrapoletor(r_data,dr,future,plus)
+        media = round((media-1)*100)
+        fig.add_trace(go.Scatter(x=future_r, 
+                           y=extrapolator_r,
+                           mode='lines+markers',
+                           name='Recovereds {}%'.format(media)), row=3,col=1)
+    except:
+        print("Não tem dados suficientes ainda")
+    #coloca o gráfico em base logarítmica
+    fig.update_layout(xaxis_type="log", yaxis_type="log")
+    #salva a imagem
+    fig.write_html('fig_extrapolate.html', auto_open=True)
+
         
 #cria lista com numero de casos e dia desde a primeira confirmação do caso   
 def lists(df):
@@ -174,71 +235,20 @@ def treatment(country):
     
     return confirmed, deaths, recovered
 
-def extrapolate(country='Brazil'):
-
-    #cria os datasets já organizados    
-    confirmed, deaths, recovered = treatment(country)
-    
-    #Chama a função para listar apenas quando começar a aparecer casos confirmados e começar a contagem de dias
-    c_data, dc = lists(confirmed)
-    d_data, dd = lists(deaths)
-    r_data, dr = lists(recovered)
-    
-    #cria a imagem para ser plotada com 3 linhas e 1 coluna
-    fig = make_subplots(rows=3, cols=1,
-                        subplot_titles=("Confirmed", "Deaths", "recovered"))
-        
-    #adiciona o grafico de confirmados do país a imagem
-    fig.add_trace(go.Scatter(x=dc, 
-                           y=c_data,
-                           mode='lines+markers',
-                           name=country), row=1,col=1)
-    #adiciona o grafico de fatais do país a imagem
-    fig.add_trace(go.Scatter(x=dd, 
-                           y=d_data,
-                           mode='lines+markers',
-                           name=country), row=2,col=1)
-        #adiciona o grafico de recuperados do país a imagem
-    fig.add_trace(go.Scatter(x=dr, 
-                           y=r_data,
-                           mode='lines+markers',
-                           name=country), row=3,col=1)
-    
-    #fator de aumento por dia
-    for k in (1.05,1.10,1.15,1.20):
-        extrapolator_c,future_c  = extrapoletor(c_data,dc,k)
-        extrapolator_d, future_d = extrapoletor(d_data,dd,k)
-        extrapolator_r, future_r = extrapoletor(r_data,dr,k)
-        
-        #Adiciona as funções aos graficos
-        fig.add_trace(go.Scatter(x=future_c, 
-                           y=extrapolator_c,
-                           mode='lines+markers',
-                           name='k={}'.format(k)), row=1,col=1)
-        fig.add_trace(go.Scatter(x=future_d, 
-                           y=extrapolator_d,
-                           mode='lines+markers',
-                           name='k={}'.format(k)), row=2,col=1)
-        fig.add_trace(go.Scatter(x=future_r, 
-                           y=extrapolator_r,
-                           mode='lines+markers',
-                           name='k={}'.format(k)), row=3,col=1)
-    #coloca o gráfico em base logarítmica
-    fig.update_layout(xaxis_type="log", yaxis_type="log")
-    #salva a imagem
-    fig.write_html('fig_comparation.html', auto_open=True)
-
-        
-
-def extrapoletor(data,days,plus):
+       
+#Extrapola aumentando com a média de aumento dos casos nos ultimos 4 dias
+def extrapoletor(data,days,future,plus=1):
+    media = (((data[-1]/data[-2])-1) + ((data[-2]/data[-3])-1) + ((data[-3]/data[-4])-1)+ ((data[-4]/data[-5])-1))/4
+    media = media * plus
+    media += 1
     today = data[-1]
     extra = [today]
-    for x in range(30):
-        i = today*plus
+    for x in range(len(days)-1,len(days)+future):
+        i = today*media
         extra.append(i)
         today = i
-    days = list(range(len(days),len(days)+30))
-    return extra, days
+    day = list(range(len(days)-1,len(days)+future))
+    return extra, day , media
         
         
         
