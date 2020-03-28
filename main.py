1d51951b6a27fd4542aa2aea5480c84eaa9722a9
Leonardo Ribeiro Solano <61062447+lrsolano@users.noreply.github.com>
2020-03-28 16:13:17 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from random import randint
+import plotly.express as px
 
 #Gráficos relacionados a um país
 def covid_19_country(country='Brazil',extra=0):
@@ -16,7 +17,7 @@ def covid_19_country(country='Brazil',extra=0):
                         subplot_titles=("Confirmed", "Deaths", "recovered"))
     #fator de aumento caso não se tenha certeza dos valores divulgados
     if extra == 1:
-        for k in (1.05,1.10,1.20,1.30,1.50):
+        for k in (1.05,1.10,1.20,1.30,1.50,1.70,2):
             fig.add_trace(go.Scatter(x=list(confirmed.index), 
                            y=list(confirmed.values*k),
                            mode='lines+markers',
@@ -42,7 +43,7 @@ def covid_19_country(country='Brazil',extra=0):
     fig.write_html('fig{}.html'.format(country), auto_open=True)
           
 #compara pela data varios países 
-def covid_world(countrys=['Brazil','Japan','China','Italy','US']):
+def covid_world(countrys=['Brazil','Spain','China','Italy','US']):
     
     
     #cria a imagem para ser plotada com 3 linhas e 1 coluna
@@ -84,7 +85,7 @@ def covid_world(countrys=['Brazil','Japan','China','Italy','US']):
     
     
 #compara os dias de infecção de varios países   
-def covid_19_comparation(countrys=['Brazil','Japan','China','Italy','US']): 
+def covid_19_comparation(countrys=['Brazil','Spain','China','Italy','US']): 
     
     #cria a imagem para ser plotada com 3 linhas e 1 coluna
     fig = make_subplots(rows=3, cols=1,
@@ -189,6 +190,38 @@ def extrapolate(country='Brazil',plus=1,future=30):
     fig.update_layout(xaxis_type="log", yaxis_type="log")
     #salva a imagem
     fig.write_html('fig_extrapolate.html', auto_open=True)
+    
+    
+def new_cases(countrys=['Brazil','Spain','China','Italy','US']):
+    #cria a imagem para ser plotada com 3 linhas e 1 coluna
+    
+    
+    #cria a repetição na lista de países
+    df = pd.DataFrame(columns=['country','confirmed','news','days'])
+    for c in countrys:
+        
+        confirmed, deaths, recovered = treatment(c)
+        day = 0
+        yesterday = 0
+        for x in confirmed:
+            news = x - yesterday
+            new_row = {'country':c, 'confirmed':x, 'news':news,'days':day}
+            df = df.append(new_row,ignore_index=True)
+            day += 1
+            yesterday = x
+                
+        
+     
+        
+    #adiciona o grafico de confirmados do país a imagem
+    fig = px.line(df, x="days", y="news", color="country",
+           hover_name="country", log_x=True, log_y=True, range_y=[10,20000])
+    
+    
+    
+    #salva a imagem
+    fig.write_html('fig_new_cases.html', auto_open=True)
+        
 
         
 #cria lista com numero de casos e dia desde a primeira confirmação do caso   
@@ -196,27 +229,25 @@ def lists(df):
     
     #cria a lista para o dataset
     data = []
-    
-    #laço de repetição nos dados
-    for x in df.index:
-        
+
+
+    for x in df.index:            
         #verifica se já tem casos confirmados
-        if int(df[x]) > 0:
-            
+        if int(df[x]) > 0:                
             #adiciona o valor a lista de casos
             data.append(int(df[x]))
-    
+
     #cria uma lista com o intervalo de dias desde o primeiro caso
     days = list(range(0,len(data)))
-    
+
     return data,days
 
 def treatment(country):
     
     #abre os arquivos de dataset
-    df1 = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv',delimiter=',')
-    df2 = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv',delimiter=',')
-    df3 = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv',delimiter=',')
+    df1 = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv',delimiter=',')
+    df2 = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv',delimiter=',')
+    df3 = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv',delimiter=',')
     
     #filtra o país selecionado dos dataset
     confirmed = df1[df1['Country/Region']==country]
@@ -244,7 +275,7 @@ def extrapoletor(data,days,future,plus=1):
     today = data[-1]
     extra = [today]
     for x in range(len(days)-1,len(days)+future):
-        i = today*media
+        i = round(today*media)
         extra.append(i)
         today = i
     day = list(range(len(days)-1,len(days)+future))
